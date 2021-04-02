fdaf121b4703d982ac3709166e6008b26e7868a0
marco88cappelli <39903707+marco88cappelli@users.noreply.github.com>
2021-04-03 01:07:54 +0200

    Add files via upload

---
+++ mButton.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef MBUTTON.h
+#define	MBUTTON.h
+
+#if defined(ARDUINO) && ARDUINO>=100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif // defined(ARDUINO) && ARDUINO>=100
+
+#include <stddef.h>
+
+typedef void(*funRelease)(long long tmp_pressButton);
+typedef void(*funPress)();
+
+class mButton
+{
+private:
+	int pin;	//Pin relativo al pulsante
+	int oldState;
+	
+	long long dbTime = 25;	//Tempo di debounce
+	long long time = 0;
+	long long timePress = 0;
+
+	bool pulUp = 0;
+	bool chekPress = false;
+
+	funRelease buttonRelease;
+	funPress buttonPress;
+	funPress buttonChange;
+
+public:
+	bool begin(int btPin, long long btDbTime = 25, bool btPulUp = 0, funRelease funButtonRelease = NULL, funPress funButtonPress = NULL, funPress funButtonChange = NULL);
+	void checkStateButton();
+};
+#endif // !MBUTTON.h
