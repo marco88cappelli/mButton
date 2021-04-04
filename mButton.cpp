3c6fa71bc77d04c53c298df13495975a43d6d5f0
marco88cappelli <39903707+marco88cappelli@users.noreply.github.com>
2021-04-04 23:20:41 +0200

    Update mButton.cpp

--- mButton.cpp
+++ mButton.cpp
@@ -1,7 +1,8 @@
 #include "mButton.h"
 
 bool mButton::begin(int btPin, long long btDbTime = 25, bool btPulUp = 0, funRelease funButtonRelease = NULL, funPress funButtonPress = NULL, funPress funButtonChange = NULL) {
-	oldState = (btPulUp == 1) ? 0 : 1;
+	oldState = (btPulUp == 1) ? 1 : 0;
+	state = oldState;
 	pin = btPin;
 	dbTime = btDbTime;
 	buttonChange = funButtonChange;
@@ -9,24 +10,8 @@ bool mButton::begin(int btPin, long long btDbTime = 25, bool btPulUp = 0, funRel
 	buttonRelease = funButtonRelease;
 	pulUp = btPulUp;
 
-#ifdef DEBUG_BUTTON
-	Serial.println("\tDEBUG CLASS BUTTON:");
-	Serial.print("\t\t-PIN:\t");
-	Serial.println(pin);
-	Serial.print("\t\t-OLD STATE:\t");
-	Serial.println(oldState);
-	Serial.print("\t\t-PULLUP:\t");
-	Serial.println(pulUp);
-	Serial.print("\t\t-DEBOUNCE TIME:\t");
-	Serial.println(dbTime);
-#endif // DEBUG_BUTTON
-
 
 	if (buttonChange == NULL && buttonPress == NULL && buttonRelease == NULL) {
-#ifdef DEBUG_BUTTON
-		Serial.println("\tERRORE:");
-		Serial.print("\t\t-Passare almeno una funzione");
-#endif // DEBUG_BUTTON
 
 		return false;
 	}
@@ -34,39 +19,40 @@ bool mButton::begin(int btPin, long long btDbTime = 25, bool btPulUp = 0, funRel
 
 	pinMode(pin, (pulUp == 1) ? INPUT_PULLUP : INPUT);
 
-#ifdef DEBUG_BUTTON
-	Serial.println("\tPulsante inizializzato Correttamente");
-#endif // DEBUG_BUTTON
-
 	return true;
 
 }
 
 void mButton::checkStateButton() {
-	int stateButton = digitalRead(pin);
 
-	if ((!chekPress) && (oldState != stateButton)) {
-		chekPress =!chekPress ;
-		time = millis();
-	}
-
-	if (chekPress) {
-		if ((millis() - time) > dbTime) {
-			if (oldState != stateButton) {
-
-				if (buttonChange != NULL) buttonChange();
-
-				if (stateButton == !pulUp) {
-					timePress = millis();
-					if (buttonPress != NULL) buttonPress();
-				}
-
-				if ((stateButton == pulUp) && (buttonPress != NULL)) buttonRelease((long long)(millis() - timePress));
-
-				oldState = stateButton;
+		if (!chekPress) {
+			state = digitalRead(pin);
+			delay(50);
+			if (state != oldState) {
+				time = millis();
+				chekPress = !chekPress;
 			}
 
 		}
-		chekPress = false;
-	}
+
+		if (chekPress) {
+			if ((millis() - time) > dbTime) {
+				state = digitalRead(pin);
+				delay(50);
+				if (oldState != state) {
+
+					if (buttonChange != NULL) buttonChange();
+
+					if (state == !pulUp) {
+						timePress = millis();
+						if (buttonPress != NULL) buttonPress();
+					}
+
+					if ((state == pulUp) && (buttonPress != NULL)) buttonRelease((long long)(millis() - timePress));
+
+					oldState = state;
+				}
+				chekPress = false;
+			}
+		}
 }
